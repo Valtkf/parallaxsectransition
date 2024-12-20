1257071e33c3d99a48adf681e2bb69c749d7af8d
Valtkf <valentin.vgregorio@gmail.com>
2024-12-20 23:41:04 +0100

    ✨ F01 : Add effect parallax section transition

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,71 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useRef } from "react";
+import Pic1 from "../../public/images/paysage-asie.jpg";
+import Pic2 from "../../public/images/guerrier-asie.jpg";
+import { useScroll, useTransform, motion, MotionValue } from "motion/react";
+import Lenis from "lenis";
+
+// Typage de la référence
 export default function Home() {
+  const container = useRef<HTMLElement | null>(null); // Typage explicite
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start start", "end end"],
+  });
+
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time: number) {
+      // Typage explicite du paramètre
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="text-6xl font-bold">Scroll Perspective</main>
-    </div>
+    <main ref={container} className="relative h-[200vh]">
+      <Section1 scrollYProgress={scrollYProgress} />
+      <Section2 scrollYProgress={scrollYProgress} />
+    </main>
   );
 }
+
+// Typage des props pour les sections
+interface SectionProps {
+  scrollYProgress: MotionValue<number>; // Utilise `any` temporairement ou remplace par le type approprié si tu as plus d'informations
+}
+
+const Section1 = ({ scrollYProgress }: SectionProps) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [0, -5]);
+  return (
+    <motion.div
+      style={{ scale, rotate }}
+      className="sticky top-0 h-screen bg-[#C72626] text-[3.5vw] flex flex-col items-center justify-center text-white pb-[10vh]"
+    >
+      <p>Scroll Perspective</p>
+      <div className="flex gap-4">
+        <p>Section</p>
+        <div className="relative w-[12.5vw]">
+          <Image src={Pic1} alt="img" placeholder="blur" fill />
+        </div>
+        <p>Transition</p>
+      </div>
+    </motion.div>
+  );
+};
+
+const Section2 = ({ scrollYProgress }: SectionProps) => {
+  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [-5, 0]);
+  return (
+    <motion.div style={{ scale, rotate }} className="relative h-screen">
+      <Image src={Pic2} alt="img" placeholder="blur" fill />
+    </motion.div>
+  );
+};
